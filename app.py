04c0895ec0f8e22f14b869935f76741299d8df71
Woodenman23 <josephrfoster@protonmail.com>
2022-04-06 15:39:01 -0500

    6/4/2022

--- app.py
+++ app.py
@@ -18,4 +18,12 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
-    return render_template("landing.html")
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
