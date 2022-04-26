48af0963d3450bce79d599b16810aea2aeaa5c17
Woodenman23 <josephrfoster@protonmail.com>
2022-04-26 16:28:40 -0700

    reconfigured

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,42 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import login_user, logout_user, login_required, current_user
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 
+
+auth= Blueprint('auth', __name__)
+
+@auth.route("/")
+def home():
+    return render_template("index.html")
+
+
+@auth.route("/register")
+def register():
+    return render_template("register.html")
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    #session.clear()
+
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            flash("Must provide username", category = 'error')
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("must provide password", category='error')
+
+        # Query database for username
+       # rows = c.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+           flash("Invalid username and/or password", category='error')
+
+        # Remember which user has logged in
+      #  session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
